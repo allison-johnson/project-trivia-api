5b472b9d82bd1e1bd2666c7be58ec18af93192b0
allison_johnson <allison.cuttler@gmail.com>
2020-07-29 21:32:53 -0400

    post and delete actions for category

--- controllers/category.js
+++ controllers/category.js
@@ -6,5 +6,26 @@ module.exports = {
             .then(categories => {
                 resp.json(categories)
             })
+    },
+
+    showId: (req, resp) => {
+        Category.findById(req.params.id)
+            .then(category => {
+                resp.json(category)
+            })
+    },
+
+    create: (req, resp) => {
+        Category.create(req.body)
+            .then(category => {
+                resp.json(category)
+            })
+    },
+
+    delete: (req, resp) => {
+        Category.findByIdAndDelete(req.params.id)
+            .then(category => {
+                resp.json(category)
+            })
     }
 }

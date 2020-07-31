1002ae0eeeb5488ac715245c4472590f02f92138
allison_johnson <allison.cuttler@gmail.com>
2020-07-31 15:26:51 -0400

    fiddling with cors again

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express();
 const cors = require('cors')
-//router.use(cors());
+//To use or not to use?
+router.use(cors());
 
 router.use('/triviaQuestion', require('./triviaQuestion'));
 router.use('/category', require('./category'))
